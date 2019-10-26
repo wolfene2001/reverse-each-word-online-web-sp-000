1492c87fcc22c5152d10317660095e00653d648b
wolfene2001 <wolfene2001@yahoo.com>
2019-10-26 13:47:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,14 @@ def reverse_each_word(string)
    string.join(" ")
 end
 end
+
+def reverse_each_word(string)
+
+  string = string.split(" ")
+#  backwards = []
+  string.collect do |char|
+  string.reverse |char|
+
+   string.join(" ")
+end
+end
