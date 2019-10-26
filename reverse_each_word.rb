0042695fdb4396f2aeccda63acca0b26cc4724dd
wolfene2001 <wolfene2001@yahoo.com>
2019-10-26 14:55:32 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,21 @@
 def reverse_each_word(string)
 
-  string = string.split(" ")
-  backwards = []
-  string.each do |char|
-  string.reverse |char|
-  end
-  return backwards.join(" ")
+  array = string.split(" ")
+#  backwards = []
+  array.collect do |word|
+    word.reverse
+#  backwards.push(word.reverse)
+end.join(" ")
 end
+
+
+# def reverse_each_word(string)
+#
+#   string = string.split(" ")
+# #  backwards = []
+#   string.collect do |char|
+#   string.reverse |char|
+#
+#    string.join(" ")
+# end
+# end
